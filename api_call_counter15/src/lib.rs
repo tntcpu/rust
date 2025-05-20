60dc012fa29cda8f9f6fba3086b7fffe9e11e3ad
tntcpu <gpscpu@gmail.com>
2025-05-20 17:33:22 +0800

    refcell feature

--- api_call_counter15/src/lib.rs
+++ api_call_counter15/src/lib.rs
@@ -1,41 +1,37 @@
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        // sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
-    impl MockMessenger{
+    impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: vec![],
+                sent_messages: RefCell::new(vec![]),
             }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
-
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
     #[test]
     fn it_sends_an_over_75_percent_warning() {
         let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(
-            &mock_messenger,
-            100
-        );
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
-
-
 
 pub trait Messenger {
     fn send(&self, msg: &str);
