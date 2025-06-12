4ad9ab08781f4ab44dda2e94650e5cba6078dc3b
tntcpu <gpscpu@gmail.com>
2025-06-12 14:04:15 +0800

    concurrency_counter_share_arc16

--- concurrency_counter_share_arc16/src/main.rs
+++ concurrency_counter_share_arc16/src/main.rs
@@ -1,7 +1,14 @@
-use std::sync：：{Arc, Mutex};
+use std::sync::{Arc,Mutex};
 use std::thread;
 fn main() {
-    let counter = Arc::new(Mutex::new（0）);
-    let mut handles = vec![];
-    
+    let counter = Arc::new(Mutex::new(0));
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+    }
+    thread::sleep(std::time::Duration::from_millis(100));
+    println!("Result: {}", *counter.lock().unwrap());
 }
