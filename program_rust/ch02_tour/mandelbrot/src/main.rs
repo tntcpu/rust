d1b4f23a4f987c1a54e658fbcde4b3f544d60b18
tntcpu <tntcpu@126.com>
2025-08-26 23:56:04 +0800

    mandelbrot

--- program_rust/ch02_tour/mandelbrot/src/main.rs
+++ program_rust/ch02_tour/mandelbrot/src/main.rs
@@ -1,7 +1,31 @@
+use image::ColorType;
+use image::png::PNGEncoder;
 use num::Complex;
+use num::traits::bounds;
+use std::env;
+use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn complex_square_add_loop(c: Complex<f64>) {
@@ -89,21 +113,37 @@ fn test_pixel_to_point() {
             Complex { re: -1.0, im: 1.0 },
             Complex { re: 1.0, im: -1.0 }
         ),
-        Complex { re: -0.5, im: -0.75 }
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
     );
 }
 
-fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
-            pixels[row * bounds.0 + column] =
-                match escape_time(point, 255) {
-                    None => 0,
-                    Some(count) => 255 - count as u8,
-                };
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
         }
     }
+}
+
+fn write_image(filename: &str, pixel: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixel, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
